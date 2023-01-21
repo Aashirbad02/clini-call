4eab49ba2cbc5256e01b488c3d5b1bd48cd68c7a
Aashirbad Sabat <aum.aashirbad@gmail.com>
2023-01-21 16:58:24 +0530

    Frontend Initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(cookieParser());
@@ -9,20 +10,15 @@ dotenv.config({ path: "./config.env" });
 
 require("./db/conn");
 
-//Convert JSON to objects
 app.use(express.json());
-
-// We link the router files to make our route easy
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 app.use(require("./router/auth"));
 
 const PORT = process.env.PORT || 5000;
-
-// app.get("/signin", (req, res) => {
-//   res.send("Login");
-// });
-// app.get("/signup", (req, res) => {
-//   res.send("Register Here");
-// });
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
