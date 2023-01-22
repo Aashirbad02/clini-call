a95e0c3a6a9777697b9f2e64d1b9accc11f3a2c6
Anubhab <anubhabpatra02@gmail.com>
2023-01-22 09:04:19 +0530

    login and signup page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import {Routes,Route} from 'react-router-dom'
+import Signup from "./components/signup";
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Hero/>
+      <Routes>
+        <Route exact path='/hero' element={<Hero/>}> </Route>
+        {/* <Route exact path='/about' element={<About/>}> </Route> */}
+        <Route exact path='/login' element={<Login/>}> </Route>
+        <Route exact path='/signup' element={<Signup/>}> </Route>
+        {/* <Route exact path='/blood' element={<Blood/>}> </Route>
+        <Route exact path='/doctor' element={<Doctor/>}> </Route> */}
+      </Routes>
+      {/* <Doctors/> */}
     </div>
   );
 }
